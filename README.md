c5039691a97ef22833fa15927bb8d9fe5e9e8743
moder <korpa.peter0723@gmail.com>
2025-05-10 16:45:55 +0200

    Initial commit (Added base java classes)

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # SQAT-Selenium-Exercise
-Selenium test of http://liftzone.hu/
+
+This repositiory contains a project for the (SQAT)[https://jacksonfurrier.github.io/SQAT/]  ELTE-Msc course. The website under test is http://liftzone.hu/.
+
+
+
+Todos
+- Add doc comments where needed
+- Write funcionality classes for 7 classes, also what to test on them
+  - Main page (http://liftzone.hu/)
+    - 8. Send a form - Lap alján visszajelzés
+    - 7. Fill input (text,radio,check...) - visszajelzés küldése előtt teszt email-cím field
+    - 6. Logout - kilépés a főoldalról
+  - Login page (http://liftzone.hu/?o=loginform)
+    - 9. Static Page test - a login page megnyitása
+    - 4. Fill simple form and send (eg. Login) - Belépés
+  - Registration page (http://liftzone.hu/?o=registerform)
+    - 8. Send a form - Edző értékelés
+    - 7. Fill input (text,radio,check...) - csillagos értékelés
+  - Shop (http://liftzone.hu/?o=shop)
+  - Edzők oldal (http://liftzone.hu/?o=edzokall)
+  - Fiók (http://liftzone.hu/?o=fiok)
+    - 5. Form sending with user - Felhasználó adatok változtatása
+  - Időpont foglalás (http://liftzone.hu/foglalas/?eid=1)
+
+Megj.: 
+- Send form/fill inputból ból több is lehet
+- 
---
+++ src/test/java/PageBase.java
@@ -0,0 +1,50 @@
+import org.junit.*;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+
+/**
+* PageBase is the base class for all Selenium tests which test a specific page.
+* <p>
+* @author      Korpa Peter
+* @author      Hudoba Peter
+* @since       1.0
+*/
+class PageBase {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    // This can be freely set by the user of the class/subclasses so there are no setter/getters
+    public int waitAmount = 10;
+    
+    public PageBase(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, waitAmount);
+    }
+    /**
+    * Wait until a speficit element is loaded then return the fouond element.
+    * <p>
+    * @param  locator  a term of type By speficying which element to wait for and return 
+    * @return      the element found by locator
+    */
+    protected WebElement waitAndReturnElement(By locator) {
+        this.wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        return this.driver.findElement(locator);
+    } 
+    /**
+    * Returns the text content of the curret active page via *getText()*
+    * <p>
+    * @return      the text content of the current page
+    */
+    public String getBodyText() {
+        WebElement bodyElement = this.waitAndReturnElement(By.tagName("body"));
+        return bodyElement.getText();
+    }
+   
+}
